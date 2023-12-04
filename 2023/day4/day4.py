79581ccafad27b4f6f9967bf96e7292cf3bdd8a5
Carl Corneil <aecorn@gmail.com>
2023-12-04 18:05:46 +0100

    shorten

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,27 +1,7 @@
 with open("input.txt", "r") as inp_file:
     content = inp_file.read()
-
-
-# Part1
-part1_sum = 0
-for line in content.split("\n"):
-    remove_game = line.split(":")[1].strip()
-    winning = [int(x) for x in remove_game.split("|")[0].strip().split(" ") if x]
-    draws = [int(x) for x in remove_game.split("|")[1].strip().split(" ") if x]
-    overlap = [x for x in draws if x in winning]
-    #print(f"{overlap=}")
-    score = 0
-    if overlap:
-        score = 1
-        overlap.pop(0)
-    for _ in overlap:
-        score *= 2
-    #print(score)
-    part1_sum += score
-print("Part1:", part1_sum)
-
-# Part2
 count_each_card = [1 for _ in content.split("\n")]
+part1_sum = 0
 for i, line in enumerate(content.split("\n")):
     remove_game = line.split(":")[1].strip()
     winning = [int(x) for x in remove_game.split("|")[0].strip().split(" ") if x]
@@ -31,5 +11,12 @@ for i, line in enumerate(content.split("\n")):
         for k in range(i+1, i+len(overlap)+1):
             #print(i, j, k)
             count_each_card[k] += 1
-#print(count_each_card)
+    score = 0
+    if overlap:
+        score = 1
+        overlap.pop(0)
+    for _ in overlap:
+        score *= 2
+    part1_sum += score
+print("Part1:", part1_sum)
 print("Part2:", sum(count_each_card))
